91f18dec1dfbf9a633c100ebca78377e5f471ec8
codewithAdi04 <adityakumarkolesh620@gmail.com>
2025-01-07 17:42:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '1c53aa6394mshee39ebdd73f520ap1c8980jsn69b141fcd9ba',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
